3fca4f853ac4c93d64e3b0e563e6608b61fee6a2
Siddharth Chopda <siddharthchopda@Siddharths-MacBook-Air.local>
2025-04-05 18:31:40 +0530

    chore: frontend repo setup with tailwind

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bg-gray-100 min-h-screen ">
+    <h1 class="text-4xl font-bold text-center text-blue-500">Task Manager</h1>
+  </div>
+</template>
